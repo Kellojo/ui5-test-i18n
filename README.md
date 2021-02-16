9413bc5fdc54273e91dd8a3c887a0de0dfde54d1
Manuel.koehrer@web.de <kellojo1@gmail.com>
2021-02-16 19:29:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ui5-test-i18n
 A small cli that cross checks your ui5 message bundles for missing translations.
+
+
+# Local Development Setup
+1. Clone the repository
+2. navigate into the root of the repo
+3. run `npm link` in the root dir
+4. you can now use `ui5-test-i18n` as a command locally
---
+++ cli.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
+const propertiesReader = require("properties-reader");
+const { exit } = require("process");
+
+
+
+const [,, ...args] = process.argv
+
+
+
+
+/**
+ * Finds all .properties fiels in a given directory
+ * @param {string} sPath 
+ * @private
+ */
+const findPropertiesFiles = function(sPath) {
+    let files = fs.readdirSync(sPath);
+    return files.filter( file => file.match(new RegExp(`.*\.(.properties)`, 'ig')));
+}
+
+/**
+ * Get's a list of all properties in a given .properties file
+ * @param {string} sFilePath 
+ * @returns {string[]}
+ * @private
+ * 
+ */
+const getKeysFromPropertiesFile = function(sFilePath) {
+    let aKeys = [];
+    propertiesReader(sFilePath).each((sKey, sValue) => {
+        aKeys.push(sKey);
+    });
+    return aKeys;
+}
+
+/**
+ * Get's a list of all properties in a given .properties file
+ * @param {string} sFilePath - the path to the file
+ * @param {string[]} aDesiredProperties - the properties the file should have
+ * @returns {boolean}
+ * @private
+ */
+const doesFileMissAnyProperties = function(sFilePath, aDesiredProperties) {
+    let aFileKeys = getKeysFromPropertiesFile(sFilePath);
+    let missesAnyProperties = false;
+    let iMissingKeys = 0;
+
+
+    console.log(chalk.underline(`\nChecking ${sFilePath} for missing properties`));
+
+    aDesiredProperties.forEach(sDesiredKey => {
+        if (!aFileKeys.includes(sDesiredKey)) {
+            console.log(chalk.redBright(`Property "${sDesiredKey}" does not exist in ${sFilePath}`));
+
+            missesAnyProperties = true;
+            iMissingKeys++;
+        }
+    });
+
+    console.log(`${ iMissingKeys == 0 ? '✅' : '❌'} Found ${iMissingKeys} missing properties`);
+
+
+    return missesAnyProperties;
+}
+
+const checkForMissingKeys = function(sScanDir) {
+
+    console.log(chalk.bold("Starting ui5-test-i18n"));
+
+    // check that path exists
+    if (!fs.existsSync(sScanDir)) {
+        console.log(chalk.redBright(`Scan directory ${sScanDir} does not exist`));
+        process.exit(1);
+    }
+
+    // get all files
+    console.log(`Looking for .properties files in ${sScanDir}`);
+    const aFiles = findPropertiesFiles(sScanDir);
+    console.log(`Found ${aFiles.length} .properties files`);
+
+    // get all keys from file
+    var aPorperties = [];
+    aFiles.forEach(sFile => {
+        const sFilePath = path.join(sScanDir, sFile);
+        const aCurrentPorperties = getKeysFromPropertiesFile(sFilePath);
+        console.log(`Found ${aCurrentPorperties.length} translations in ${sFile}`);
+
+        // add to collection, is not already present
+        aCurrentPorperties.forEach(sKey => {
+            if (!aPorperties.includes(sKey)) {
+                aPorperties.push(sKey);
+            }
+        });
+    });
+
+    
+    // cross check every file for missing key
+    let bErrorsFound = false;
+    aFiles.forEach(sFile => {
+        const sFilePath = path.join(sScanDir, sFile);
+        if (doesFileMissAnyProperties(sFilePath, aPorperties)) {
+            bErrorsFound = true;
+        }
+    });
+
+    process.exit(bErrorsFound ? 1 : 0);
+}
+
+
+
+checkForMissingKeys(args[0]);
